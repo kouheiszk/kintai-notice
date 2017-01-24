6cff4a5f1e835b40060697c4a290dabdb77c2e38
Kouhei Suzuki <kouhei.szk@gmail.com>
2017-01-24 19:03:03 +0900

    ログが保存されるようにする

--- notifier.rb
+++ notifier.rb
@@ -10,7 +10,16 @@ class Settings < Settingslogic
   source "#{ROOT}/config/settings.yml"
 end
 
-notifier = Slack::Notifier.new Settings.slack.webhook_uri, Settings.slack.option
-notifier.ping("@channel 先月の勤怠を入力してください\nhttps://p.secure.freee.co.jp/")
+logger = Logger.new("#{ROOT}/logs/notifer.log")
+logger.debug("Start")
+
+begin
+  notifier = Slack::Notifier.new Settings.slack.webhook_uri, Settings.slack.option
+  notifier.ping("@channel 先月の勤怠を入力してください\nhttps://p.secure.freee.co.jp/")
+rescue => e
+  logger.fatal(e)
+end
+
+logger.debug("Finish")
 
 # binding.pry
